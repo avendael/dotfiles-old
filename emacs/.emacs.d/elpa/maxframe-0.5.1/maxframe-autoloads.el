6c8f2c90626c04bf94d3f930c85c9f95e88285e0
Evan Dale Aromin <avendael@gmail.com>
2012-08-04 12:14:10 +0800

    New elpa directory for emacs24 with installed packages.

---
+++ emacs/.emacs.d/elpa/maxframe-0.5.1/maxframe-autoloads.el
@@ -0,0 +1,29 @@
+;;; maxframe-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (maximize-frame) "maxframe" "maxframe.el" (20506
+;;;;;;  55563))
+;;; Generated autoloads from maxframe.el
+
+(autoload 'maximize-frame "maxframe" "\
+Maximizes the frame to fit the display if under a windowing
+system.
+
+\(fn &optional THE-FRAME)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("maxframe-pkg.el") (20506 55563 237833))
+
+;;;***
+
+(provide 'maxframe-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; maxframe-autoloads.el ends here
