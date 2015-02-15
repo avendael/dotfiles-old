c7a8742d3b6796a64012af2d573848a59e91c834
Evan Dale Aromin <evan@avendael.com>
2015-02-15 21:58:29 +0800

    Remove emacs from dotfiles.sh and add zsh

--- dotfiles.sh
+++ dotfiles.sh
@@ -4,6 +4,5 @@ dotfiles_dir="`pwd`"
 cd ~
 ln -s ${dotfiles_dir}/bash/.bash_profile .bash_profile
 ln -s ${dotfiles_dir}/bash/.bashrc .bashrc
-ln -s ${dotfiles_dir}/emacs/.emacs .emacs
-ln -s ${dotfiles_dir}/emacs/.emacs.d .emacs.d
+ln -s ${dotfiles_dir}/zshrc .zshrc
 cd -
