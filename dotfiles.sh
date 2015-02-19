69e59cc9fded00c937938b92f6d1c1e7e4154bba
Evan Dale Aromin <evan@avendael.com>
2015-02-20 00:44:37 +0800

    Add dotctags to dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -5,4 +5,5 @@ cd ~
 ln -s ${dotfiles_dir}/bash/.bash_profile .bash_profile
 ln -s ${dotfiles_dir}/bash/.bashrc .bashrc
 ln -s ${dotfiles_dir}/zshrc .zshrc
+ln -s ${dotfiles_dir}/dotctags .ctags
 cd -
